c94f33b22f2b0a27d8ec67bda7a2b93b4e2537bf
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-05-06 13:44:51 +0800

    fix deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,19 +15,14 @@ const config = {
   log: true,
   cmds: [
     {
-      type: 'cmd',
-      args: ['rm', '-rf', 'dist'],
-      cwd: 'crashmax.ru/app/client/'
+      type: 'upload',
+      src: join(__dirname, '../dist'),
+      dest: 'crashmax.ru/app/dist/'
     },
     {
       type: 'upload',
-      src: join(__dirname, '../dist/'),
-      dest: 'crashmax.ru/app/dist'
-    },
-    {
-      type: 'upload',
-      src: join(__dirname, '../client/dist/'),
-      dest: 'crashmax.ru/app/client/dist'
+      src: join(__dirname, '../client/dist'),
+      dest: 'crashmax.ru/app/client/dist/'
     },
     {
       type: 'cmd',
