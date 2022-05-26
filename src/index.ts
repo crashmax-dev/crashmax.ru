2ca47271c72b4796a3a5fcdbfad1854289695728
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-05-26 22:34:47 +0800

    add typebox, refactor `api/email`

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,25 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import fastifyAutoload from '@fastify/autoload'
 import { joinPath } from './utils/path.js'
+import { ajvTypeBoxPlugin } from '@fastify/type-provider-typebox'
+import type { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 
 const server = fastify({
-  logger: process.env.NODE_ENV !== 'production'
-})
+  logger: {
+    enabled: process.env.NODE_ENV === 'development',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        singleLine: true,
+        ignore: 'time,pid,hostname',
+        colorize: true
+      }
+    }
+  },
+  ajv: {
+    plugins: [ajvTypeBoxPlugin]
+  }
+}).withTypeProvider<TypeBoxTypeProvider>()
 
 server.register(fastifyAutoload, {
   dir: joinPath(import.meta, 'plugins')
--- src/routes/api/email/index.ts
+++ src/routes/api/email/index.ts
@@ -1,8 +1,43 @@
-import type { FastifyPluginAsync } from 'fastify'
+import { createTransport } from 'nodemailer'
+import { Type } from '@sinclair/typebox'
+import type { Static } from '@sinclair/typebox'
+import type { FastifyPluginAsync, RouteShorthandOptions } from 'fastify'
 
-const email: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.post('/', async (request, reply) => {
-    return reply.sendMail(request)
+const body = Type.Object({
+  to: Type.String({ format: 'email' }),
+  from: Type.Optional(Type.String()),
+  subject: Type.Optional(Type.String()),
+  text: Type.String(),
+  token: Type.String()
+})
+
+type Request = {
+  Body: Static<typeof body>
+}
+
+const routeOptions: RouteShorthandOptions = {
+  schema: {
+    body
+  }
+}
+
+const email: FastifyPluginAsync = async (fastify): Promise<void> => {
+  const { EMAIL_USER, EMAIL_PASS, EMAIL_HOST, EMAIL_PORT } = process.env
+  const transporter = createTransport(
+    `smtp://${EMAIL_USER}:${EMAIL_PASS}@${EMAIL_HOST}:${EMAIL_PORT}`,
+    { from: `crashmax.ru <${EMAIL_USER}>` }
+  )
+
+  fastify.addHook<Request>('preHandler', (request, reply, done) => {
+    if (process.env.EMAIL_TOKEN !== request.body.token) {
+      throw fastify.httpErrors.unauthorized()
+    }
+
+    done()
+  })
+
+  fastify.post<Request>('/', routeOptions, async (request, reply) => {
+    return await transporter.sendMail(request.body)
   })
 }
 
