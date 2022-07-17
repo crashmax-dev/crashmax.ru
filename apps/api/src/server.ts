3eb71dc92ecf1034a6c995a0530fe7fdd709ecda
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-07-17 18:13:35 +0800

    upd

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,42 +3,49 @@ import fastifyEnv from '@fastify/env'
 import fastifyAutoload from '@fastify/autoload'
 import { envSchema } from './config.js'
 import { joinPath } from './utils/path.js'
-import { ajvTypeBoxPlugin } from '@fastify/type-provider-typebox'
 import type { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 
-export function buildServer() {
-  const fastify = Fastify({
-    trustProxy: true,
-    logger: {
-      enabled: process.env.NODE_ENV === 'development',
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          singleLine: true,
-          ignore: 'time,pid,hostname',
-          colorize: true
-        }
+const fastify = Fastify({
+  trustProxy: true,
+  logger: {
+    enabled: process.env.NODE_ENV === 'development',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        singleLine: true,
+        ignore: 'time,pid,hostname',
+        colorize: true
       }
-    },
-    ajv: {
-      plugins: [ajvTypeBoxPlugin]
     }
-  }).withTypeProvider<TypeBoxTypeProvider>()
+  }
+}).withTypeProvider<TypeBoxTypeProvider>()
 
-  fastify.register(fastifyEnv, {
-    schema: envSchema,
-    dotenv: {
-      path: joinPath(import.meta, '../.env')
+fastify.register(fastifyEnv, {
+  schema: envSchema,
+  dotenv: {
+    path: joinPath(import.meta, '../.env')
+  }
+})
+
+fastify.register(fastifyAutoload, {
+  dir: joinPath(import.meta, 'plugins')
+})
+
+fastify.register(fastifyAutoload, {
+  dir: joinPath(import.meta, 'routes')
+})
+
+const host = process.env.APP_IP ?? 'localhost'
+const port = process.env.APP_PORT ?? 8000
+
+fastify.listen(
+  { host, port },
+  (err, host) => {
+    if (err) {
+      // console.log(err)
+      return
     }
-  })
 
-  fastify.register(fastifyAutoload, {
-    dir: joinPath(import.meta, 'plugins')
-  })
-
-  fastify.register(fastifyAutoload, {
-    dir: joinPath(import.meta, 'routes')
-  })
-
-  return fastify
-}
+    console.log(host)
+  }
+)
